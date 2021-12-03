720b642d1908c17cb1675125a61be32203af4bf8
James Leslie <jamesleslie@posteo.net>
2021-12-03 13:08:34 -0500

    Changed default.nix to include dependencies.

--- default.nix
+++ default.nix
@@ -1,10 +1,8 @@
-with (import <nixpkgs> {});
-
-stdenv.mkDerivation {
-  name = "passbemenu";
-  builder = "${bash}/bin/bash";
-  args = [ ./builder.sh ];
-  inherit coreutils;
-  src = ./passbemenu.sh;
-  system = builtins.currentSystem;
-}
+let build = { runCommandLocal, lib, makeWrapper, dmenu-wayland, bemenu }:
+      runCommandLocal "passbemenu"
+        {nativeBuildInputs = [ makeWrapper ]; }
+        ''
+      makeWrapper ${./passbemenu.sh} $out/bin/passmenu --prefix PATH : ${lib.makeBinPath [ dmenu-wayland bemenu ]}
+      '';
+in with (import <nixpkgs> {});
+  build { inherit runCommandLocal lib makeWrapper dmenu-wayland bemenu; }
