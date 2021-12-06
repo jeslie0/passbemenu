6b6f33a1c16f29f2a6442f55ae4549cd80ea5eb5
James Leslie <jamesleslie@posteo.net>
2021-12-06 18:15:07 -0500

    Added all systems.

--- flake.nix
+++ flake.nix
@@ -2,17 +2,20 @@
   description = "Allows passmenu to operate with bemenu, rather than dmenu." ;
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
-    defaultPackage.x86_64-linux =
-      with import nixpkgs { system = "x86_64-linux"; };
-      stdenv.mkDerivation {
-        name = "passmenu";
-        src = ./passbemenu.sh;
-        nativeBuildInputs = [ makeWrapper ];
-        builder = ./builder.sh;
-        inherit coreutils dmenu-wayland bemenu makeWrapper;
-      };
-
-  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (
+      system: {
+        defaultPackage =
+          with import nixpkgs { inherit system; };
+          stdenv.mkDerivation {
+            name = "passmenu";
+            src = ./passbemenu.sh;
+            nativeBuildInputs = [ makeWrapper ];
+            builder = ./builder.sh;
+            inherit coreutils dmenu-wayland bemenu makeWrapper;
+          };
+      }
+    );
 }
